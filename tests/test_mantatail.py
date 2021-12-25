61a42ec4fc073d5090d4aeca0ed80ccf7c1da7fe
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-12-25 19:41:06 +0100

    Implement test of PART error msgs

--- tests/test_mantatail.py
+++ tests/test_mantatail.py
@@ -64,15 +64,15 @@ def run_server(fail_test_if_there_is_an_error_in_a_thread):
 def user_alice(run_server):
     alice_socket = socket.socket()
     alice_socket.connect(("localhost", 6667))
-    alice_socket.sendall(b"NICK foo\r\n")
+    alice_socket.sendall(b"NICK alice\r\n")
 
     # Receiving everything the server is going to send helps prevent errors.
     # Otherwise it might not be fully started yet when the client quits.
     received = b""
-    while not received.endswith(b"\r\n:mantatail 376 foo :End of /MOTD command\r\n"):
+    while not received.endswith(b"\r\n:mantatail 376 alice :End of /MOTD command\r\n"):
         received += alice_socket.recv(4096)
 
-    yield user_alice
+    yield alice_socket
     alice_socket.sendall(b"QUIT\r\n")
     alice_socket.close()
 
@@ -81,19 +81,44 @@ def user_alice(run_server):
 def user_bob(run_server):
     bob_socket = socket.socket()
     bob_socket.connect(("localhost", 6667))
-    bob_socket.sendall(b"NICK foo\r\n")
+    bob_socket.sendall(b"NICK bob\r\n")
 
     # Receiving everything the server is going to send helps prevent errors.
     # Otherwise it might not be fully started yet when the client quits.
     received = b""
-    while not received.endswith(b"\r\n:mantatail 376 foo :End of /MOTD command\r\n"):
+    while not received.endswith(b"\r\n:mantatail 376 bob :End of /MOTD command\r\n"):
         received += bob_socket.recv(4096)
 
-    yield user_bob
+    yield bob_socket
     bob_socket.sendall(b"QUIT\r\n")
     bob_socket.close()
 
 
 ##############
+#    UTILS   #
+##############
+
+
+def recv_loop(user):
+    received = b""
+    while not received.endswith(b"\r\n"):
+        received += user.recv(1)
+    return received
+
+
+##############
 #    TESTS   #
 ##############
+
+
+def test_no_such_channel(user_alice):
+    user_alice.sendall(b"PART #foo\r\n")
+    received = recv_loop(user_alice)
+    assert received == b":mantatail 403 #foo :No such channel\r\n"
+
+
+def test_youre_not_on_that_channel(user_alice, user_bob):
+    user_alice.sendall(b"JOIN #foo\r\n")
+    user_bob.sendall(b"PART #foo\r\n")
+    received = recv_loop(user_bob)
+    assert received == b":mantatail 442 #foo :You're not on that channel\r\n"
