1721f6bc61adcff9fb7ce5753e74b341feb7ac4a
Akuli <akuviljanen17@gmail.com>
2021-12-26 01:46:40 +0200

    sleep

--- tests/test_mantatail.py
+++ tests/test_mantatail.py
@@ -1,6 +1,7 @@
 import pytest
 import socket
 import threading
+import time
 from mantatail import Server
 
 motd_dict_test = {
@@ -131,7 +132,7 @@ def test_no_such_channel(user_alice):
 
 def test_youre_not_on_that_channel(user_alice, user_bob):
     user_alice.sendall(b"JOIN #foo\r\n")
-    # TODO: wait until server says that join is done, prevents randomly occurring errors
+    time.sleep(0.1)  # TODO: wait until server says that join is done
     user_bob.sendall(b"PART #foo\r\n")
     received = recv_loop(user_bob)
     assert received == b":mantatail 442 #foo :You're not on that channel\r\n"
