8ba2d9cbe8d8811f1bc738aaa07eb3d269b80ae3
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-12-25 21:10:40 +0100

    Make channels and users case insensitive

--- mantatail.py
+++ mantatail.py
@@ -10,146 +10,6 @@ from typing import Dict, Optional, Union, List
 import irc_responses
 
 
-class User:
-    def __init__(self, host: str, socket: socket.socket):
-        self.socket = socket
-        self.host = host
-        self.nick: Optional[str] = None
-        self.user_name: Optional[str] = None
-
-    def create_user_mask(self) -> str:
-        return f"{self.nick}!{self.user_name}@{self.host}"
-
-
-class Channel:
-    def __init__(self) -> None:
-        self.user_dict: Dict[Optional[str], User] = {}
-
-
-class IrcCommandHandler:
-    def __init__(self, server: Server, user: User) -> None:
-        self.encoding = "utf-8"
-        self.send_to_client_prefix = ":mantatail"
-        self.send_to_client_suffix = "\r\n"
-        self.server = server
-        self.user = user
-
-    def handle_motd(self) -> None:
-        (
-            start_num,
-            start_info,
-        ) = irc_responses.RPL_MOTDSTART
-        motd_num = irc_responses.RPL_MOTD
-        (
-            end_num,
-            end_info,
-        ) = irc_responses.RPL_ENDOFMOTD
-
-        motd_start_and_end = {
-            "start_msg": f"{self.send_to_client_prefix} {start_num} {self.user.nick} :- mantatail {start_info}{self.send_to_client_suffix}",
-            "end_msg": f"{self.send_to_client_prefix} {end_num} {self.user.nick} {end_info}{self.send_to_client_suffix}",
-        }
-
-        start_msg = bytes(motd_start_and_end["start_msg"], encoding=self.encoding)
-        end_msg = bytes(motd_start_and_end["end_msg"], encoding=self.encoding)
-
-        self.user.socket.sendall(start_msg)
-
-        if self.server.motd_content:
-            motd = self.server.motd_content["motd"]
-            for motd_line in motd:
-                motd_msg = bytes(
-                    f"{self.send_to_client_prefix} {motd_num} {self.user.nick} :{motd_line.format(user_nick=self.user.nick)}{self.send_to_client_suffix}",
-                    encoding=self.encoding,
-                )
-                self.user.socket.sendall(motd_msg)
-        # If motd.json could not be found
-        else:
-            no_motd_num, no_motd_info = irc_responses.ERR_NOMOTD
-            self.user.socket.sendall(
-                bytes(
-                    f"{self.send_to_client_prefix} {no_motd_num} {no_motd_info}{self.send_to_client_suffix}",
-                    encoding=self.encoding,
-                )
-            )
-
-        self.user.socket.sendall(end_msg)
-
-    def handle_join(self, channel_name: str) -> None:
-        channel_regex = r"#[^ \x07,]{1,49}"  # TODO: Make more restrictive (currently valid: ###, #ö?!~ etc)
-
-        if not re.match(channel_regex, channel_name):
-            self.handle_no_such_channel(channel_name)
-        else:
-            if channel_name not in self.server.channels.keys():
-                self.server.channels[channel_name] = Channel()
-
-            if (
-                self.user.nick
-                not in self.server.channels[channel_name].user_dict.keys()
-            ):
-                self.server.channels[channel_name].user_dict[self.user.nick] = self.user
-
-        # TODO: Check for:
-        #   * User invited to channel
-        #   * Nick/user not matching bans
-        #   * Eventual password matches
-        #   * Not joined too many channels
-
-    def handle_part(self, channel_name: str) -> None:
-        if channel_name not in self.server.channels.keys():
-            self.handle_no_such_channel(channel_name)
-        elif self.user.nick not in self.server.channels[channel_name].user_dict.keys():
-            (
-                not_on_channel_num,
-                not_on_channel_info,
-            ) = irc_responses.ERR_NOTONCHANNEL
-
-            self.generate_error_reply(
-                not_on_channel_num, not_on_channel_info, channel_name
-            )
-        else:
-            del self.server.channels[channel_name].user_dict[self.user.nick]
-            if len(self.server.channels[channel_name].user_dict) == 0:
-                del self.server.channels[channel_name]
-
-        # TODO: Support user writing /part without specifying channel name
-
-    def _handle_quit(self) -> None:
-        pass
-
-    def _handle_kick(self, message: str) -> None:
-        pass
-
-    def handle_nick(self, nick: str) -> None:
-        self.user.nick = nick
-
-    def handle_user(self, message: str) -> None:
-        self.user.user_name = message.split(" ", 1)[0]
-
-    def _handle_privmsg(self, message: str) -> None:
-        pass
-
-    def handle_unknown_command(self, command: str) -> None:
-        unknown_cmd_num, unknown_cmd_info = irc_responses.ERR_UNKNOWNCOMMAND
-
-        self.generate_error_reply(unknown_cmd_num, unknown_cmd_info, command)
-
-    def handle_no_such_channel(self, channel_name: str) -> None:
-        no_channel_num, no_channel_info = irc_responses.ERR_NOSUCHCHANNEL
-        self.generate_error_reply(no_channel_num, no_channel_info, channel_name)
-
-    def generate_error_reply(
-        self, error_num: str, error_info: str, error_topic: str
-    ) -> None:
-        self.user.socket.sendall(
-            bytes(
-                f"{self.send_to_client_prefix} {error_num} {error_topic} {error_info}{self.send_to_client_suffix}",
-                encoding=self.encoding,
-            )
-        )
-
-
 class Server:
     def __init__(self, port: int, motd_content: Optional[Dict[str, List[str]]]) -> None:
         self.host = "127.0.0.1"
@@ -214,6 +74,158 @@ class Server:
                         return
 
                     call_handler_function(message)
+
+
+class User:
+    def __init__(self, host: str, socket: socket.socket):
+        self.socket = socket
+        self.host = host
+        # Nick is shown in user lists etc, user_name is not
+        self.nick: Optional[str] = None
+        self.user_name: Optional[str] = None
+
+    def create_user_mask(self) -> str:
+        return f"{self.nick}!{self.user_name}@{self.host}"
+
+
+class Channel:
+    def __init__(self, channel_name) -> None:
+        self.channel_name = channel_name
+        self.user_dict: Dict[Optional[str], User] = {}
+
+
+class IrcCommandHandler:
+    def __init__(self, server: Server, user: User) -> None:
+        self.encoding = "utf-8"
+        self.send_to_client_prefix = ":mantatail"
+        self.send_to_client_suffix = "\r\n"
+        self.server = server
+        self.user = user
+
+    def handle_motd(self) -> None:
+        (
+            start_num,
+            start_info,
+        ) = irc_responses.RPL_MOTDSTART
+        motd_num = irc_responses.RPL_MOTD
+        (
+            end_num,
+            end_info,
+        ) = irc_responses.RPL_ENDOFMOTD
+
+        motd_start_and_end = {
+            "start_msg": f"{self.send_to_client_prefix} {start_num} {self.user.nick} :- mantatail {start_info}{self.send_to_client_suffix}",
+            "end_msg": f"{self.send_to_client_prefix} {end_num} {self.user.nick} {end_info}{self.send_to_client_suffix}",
+        }
+
+        start_msg = bytes(motd_start_and_end["start_msg"], encoding=self.encoding)
+        end_msg = bytes(motd_start_and_end["end_msg"], encoding=self.encoding)
+
+        self.user.socket.sendall(start_msg)
+
+        if self.server.motd_content:
+            motd = self.server.motd_content["motd"]
+            for motd_line in motd:
+                motd_msg = bytes(
+                    f"{self.send_to_client_prefix} {motd_num} {self.user.nick} :{motd_line.format(user_nick=self.user.nick)}{self.send_to_client_suffix}",
+                    encoding=self.encoding,
+                )
+                self.user.socket.sendall(motd_msg)
+        # If motd.json could not be found
+        else:
+            no_motd_num, no_motd_info = irc_responses.ERR_NOMOTD
+            self.user.socket.sendall(
+                bytes(
+                    f"{self.send_to_client_prefix} {no_motd_num} {no_motd_info}{self.send_to_client_suffix}",
+                    encoding=self.encoding,
+                )
+            )
+
+        self.user.socket.sendall(end_msg)
+
+    def handle_join(self, channel_name: str) -> None:
+        channel_regex = r"#[^ \x07,]{1,49}"  # TODO: Make more restrictive (currently valid: ###, #ö?!~ etc)
+
+        lower_channel_name = channel_name.lower()
+
+        if not re.match(channel_regex, lower_channel_name):
+            self.handle_no_such_channel(channel_name)
+        else:
+            if lower_channel_name not in self.server.channels.keys():
+                self.server.channels[lower_channel_name] = Channel(channel_name)
+
+            lower_user_nick = self.user.nick.lower()
+            if (
+                lower_user_nick
+                not in self.server.channels[lower_channel_name].user_dict.keys()
+            ):
+                self.server.channels[lower_channel_name].user_dict[
+                    lower_user_nick
+                ] = self.user
+
+        # TODO: Check for:
+        #   * User invited to channel
+        #   * Nick/user not matching bans
+        #   * Eventual password matches
+        #   * Not joined too many channels
+
+    def handle_part(self, channel_name: str) -> None:
+        lower_channel_name = channel_name.lower()
+        lower_user_nick = self.user.nick.lower()
+        if lower_channel_name not in self.server.channels.keys():
+            self.handle_no_such_channel(channel_name)
+        elif (
+            lower_user_nick
+            not in self.server.channels[lower_channel_name].user_dict.keys()
+        ):
+            (
+                not_on_channel_num,
+                not_on_channel_info,
+            ) = irc_responses.ERR_NOTONCHANNEL
+
+            self.generate_error_reply(
+                not_on_channel_num, not_on_channel_info, channel_name
+            )
+        else:
+            del self.server.channels[lower_channel_name].user_dict[lower_user_nick]
+            if len(self.server.channels[lower_channel_name].user_dict) == 0:
+                del self.server.channels[lower_channel_name]
+
+        # TODO: Support user writing /part without specifying channel name
+
+    def _handle_quit(self) -> None:
+        pass
+
+    def _handle_kick(self, message: str) -> None:
+        pass
+
+    def handle_nick(self, nick: str) -> None:
+        self.user.nick = nick
+
+    def handle_user(self, message: str) -> None:
+        self.user.user_name = message.split(" ", 1)[0]
+
+    def _handle_privmsg(self, message: str) -> None:
+        pass
+
+    def handle_unknown_command(self, command: str) -> None:
+        unknown_cmd_num, unknown_cmd_info = irc_responses.ERR_UNKNOWNCOMMAND
+
+        self.generate_error_reply(unknown_cmd_num, unknown_cmd_info, command)
+
+    def handle_no_such_channel(self, channel_name: str) -> None:
+        no_channel_num, no_channel_info = irc_responses.ERR_NOSUCHCHANNEL
+        self.generate_error_reply(no_channel_num, no_channel_info, channel_name)
+
+    def generate_error_reply(
+        self, error_num: str, error_info: str, error_topic: str
+    ) -> None:
+        self.user.socket.sendall(
+            bytes(
+                f"{self.send_to_client_prefix} {error_num} {error_topic} {error_info}{self.send_to_client_suffix}",
+                encoding=self.encoding,
+            )
+        )
 
 
 def get_motd_content_from_json() -> Optional[Dict[str, List[str]]]:
