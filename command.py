d6b66fd2e046a264037703175eac3c7b2e57b86a
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2022-01-03 19:32:10 +0100

    Updates after review

--- command.py
+++ command.py
@@ -50,13 +50,13 @@ def handle_join(state: mantatail.ServerState, user: mantatail.UserConnection, ch
 
 
 def handle_part(state: mantatail.ServerState, user: mantatail.UserConnection, channel_name: str) -> None:
-    try:
-        channel = state.channels[channel_name.lower()]
-    except KeyError:
-        error_no_such_channel(user, channel_name)
-        return
-
     with state.lock:
+        try:
+            channel = state.channels[channel_name.lower()]
+        except KeyError:
+            error_no_such_channel(user, channel_name)
+            return
+
         if user not in channel.users:
             error_not_on_channel(user, channel_name)
         else:
@@ -69,7 +69,7 @@ def handle_part(state: mantatail.ServerState, user: mantatail.UserConnection, ch
 
             channel.users.discard(user)
             if len(channel.users) == 0:
-                del channel
+                del state
 
 
 def handle_mode(state: mantatail.ServerState, user: mantatail.UserConnection, mode_args: str) -> None:
@@ -168,9 +168,12 @@ def motd(motd_content: Optional[Dict[str, List[str]]], user: mantatail.UserConne
 
 
 def process_channel_modes(state: mantatail.ServerState, user: mantatail.UserConnection, args: List[str]) -> None:
-    supported_modes = ["+", "-", "o"]
-    for mode in args[1]:
-        if mode not in supported_modes:
+    supported_modes = ["o"]
+    for mode in args[1][1:]:
+        if args[1][0] not in ["+", "-"]:
+            error_unknown_mode(user, args[1][0])
+            return
+        elif mode not in supported_modes:
             error_unknown_mode(user, mode)
             return
 
--- tests/test_mantatail.py
+++ tests/test_mantatail.py
@@ -142,7 +142,7 @@ def receive_line(sock):
 
 
 # Makes it easier to assert bytes received from Sets
-def compare_sets(received_bytes, compare_with):
+def compare_if_word_match_in_any_order(received_bytes, compare_with):
     return set(received_bytes.split()) == set(compare_with.split())
 
 
@@ -269,7 +269,9 @@ def test_operator_prefix(user_alice, user_bob, user_charlie):
     while True:
         received = receive_line(user_charlie)
         if b"353" in received:
-            assert compare_sets(received, b":mantatail 353 Charlie = #foo :Charlie @Alice @Bob\r\n")
+            assert compare_if_word_match_in_any_order(
+                received, b":mantatail 353 Charlie = #foo :Charlie @Alice @Bob\r\n"
+            )
             break
 
     user_charlie.sendall(b"PART #foo\r\n")
@@ -280,7 +282,9 @@ def test_operator_prefix(user_alice, user_bob, user_charlie):
     while True:
         received = receive_line(user_charlie)
         if b"353" in received:
-            assert compare_sets(received, b":mantatail 353 Charlie = #foo :Charlie @Alice Bob\r\n")
+            assert compare_if_word_match_in_any_order(
+                received, b":mantatail 353 Charlie = #foo :Charlie @Alice Bob\r\n"
+            )
             break
 
     user_charlie.sendall(b"PART #foo\r\n")
@@ -291,7 +295,9 @@ def test_operator_prefix(user_alice, user_bob, user_charlie):
     while True:
         received = receive_line(user_charlie)
         if b"353" in received:
-            assert compare_sets(received, b":mantatail 353 Charlie = #foo :Charlie @Alice @Bob\r\n")
+            assert compare_if_word_match_in_any_order(
+                received, b":mantatail 353 Charlie = #foo :Charlie @Alice @Bob\r\n"
+            )
             break
 
 
@@ -370,3 +376,8 @@ def test_nick_already_taken(run_server):
 
     while receive_line(nc2) != b":mantatail 376 nc :End of /MOTD command\r\n":
         pass
+
+    nc2.sendall(b"QUIT\r\n")
+    while b"QUIT" not in receive_line(nc2):
+        pass
+    nc2.close()
