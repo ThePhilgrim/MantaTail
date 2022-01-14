9df5f623bd42795ea659f295c8594526bf52975c
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2022-01-14 12:19:42 +0100

    Add docstrings to mantatail.py

--- mantatail.py
+++ mantatail.py
@@ -12,6 +12,11 @@ TIMER_SECONDS = 600
 
 
 class ServerState:
+    """
+    Represents the current state of the Server.
+    Keeps track of existing channels & connected users.
+    """
+
     def __init__(self, motd_content: Optional[Dict[str, List[str]]]) -> None:
         self.lock = threading.Lock()
         self.channels: Dict[str, Channel] = {}
@@ -19,12 +24,18 @@ class ServerState:
         self.motd_content = motd_content
 
     def find_user(self, nick: str) -> UserConnection:
+        """Looks for a connected user and returns the UserConnection object corresponding to that user."""
         return self.connected_users[nick.lower()]
 
     def find_channel(self, channel_name: str) -> Channel:
+        """Looks for an existing channel and returns the Channel object corresponding to that channel."""
         return self.channels[channel_name.lower()]
 
     def delete_user(self, nick: str) -> None:
+        """
+        Removes a user from all channels of which the user is connected to,
+        thereafter removes user from dict of connected users.
+        """
         user = self.connected_users[nick.lower()]
         for channel in self.channels.values():
             if user in channel.users:
@@ -32,10 +43,13 @@ class ServerState:
         del self.connected_users[nick.lower()]
 
     def delete_channel(self, channel_name: str) -> None:
+        """Removes channel from dict of existing channels."""
         del self.channels[channel_name.lower()]
 
 
 class Listener:
+    """Starts the server and listens for incoming connections from clients."""
+
     def __init__(self, port: int, motd_content: Optional[Dict[str, List[str]]]) -> None:
         self.host = "127.0.0.1"
         self.port = port
@@ -46,6 +60,11 @@ class Listener:
         self.state = ServerState(motd_content)
 
     def run_server_forever(self) -> None:
+        """
+        Accepts incoming connections from clients.
+        Starts a receive loop on a separate thread,
+        listening for incoming commands from client.
+        """
         print(f"Mantatail running ({self.host}:{self.port})")
         while True:
             (user_socket, user_address) = self.listener_socket.accept()
@@ -56,22 +75,40 @@ class Listener:
 
 
 def close_socket_cleanly(sock: socket.socket) -> None:
-    # https://blog.netherlabs.nl/articles/2009/01/18/the-ultimate-so_linger-page-or-why-is-my-tcp-not-reliable
+    """
+    Ensures that the connection to a client is closed cleanly without errors.
+
+    The code is based on this blog post:
+    https://blog.netherlabs.nl/articles/2009/01/18/the-ultimate-so_linger-page-or-why-is-my-tcp-not-reliable
+
+    Potentially raises OSError. Possible causes:
+    - Client decided to keep its connection open for more than 10sec.
+    - Client was already disconnected.
+    - Probably something else too that I didn't think of...
+    """
     try:
         sock.shutdown(socket.SHUT_WR)
         sock.settimeout(10)
         sock.recv(1)  # Wait for client to close the connection
     except OSError:
-        # Possible causes:
-        #   - Client decided to keep its connection open for more than 10sec.
-        #   - Client was already disconnected.
-        #   - Probably something else too that I didn't think of...
         pass
 
     sock.close()
 
 
 def recv_loop(state: ServerState, user_host: str, user_socket: socket.socket) -> None:
+    """
+    Instantiates UserConnection and listens for commands/messages from the client,
+    parses them and sends them to appropriate "handle_" function in "commands".
+
+    IRC Messages are formatted "bytes(COMMAND args\r\n)"
+    Ex: b"JOIN #foo\r\n"
+    Ex: b"PRIVMSG #foo :This is a message\r\n"
+
+    Note that netcat uses "\n" in place of "\r\n".
+
+    Currently only UTF-8 is supported.
+    """
     _user_message = None
     _nick = None
 
@@ -80,7 +117,6 @@ def recv_loop(state: ServerState, user_host: str, user_socket: socket.socket) ->
     try:
         while True:
             request = b""
-            # IRC messages always end with b"\r\n" (netcat uses "\n")
             while not request.endswith(b"\n"):
                 # Temporary check until these actions can be done before user registration
                 if user:
@@ -137,13 +173,26 @@ def recv_loop(state: ServerState, user_host: str, user_socket: socket.socket) ->
 
 
 class UserConnection:
+    """
+    Represents the connection between server & client.
+    Starts a Queue on a separate thread on which the client receives all messages from the server.
+
+    Format examples:
+    - Nick: Alice
+    - User message: AliceUsr 0 * Alice's Real Name
+    - Username: AliceUsr
+    - User Mask Alice!AliceUsr@127.0.0.1 (Nick!Username@Host)
+
+    All references to UserConnection (lists, dicts, etc.) are based on Nick.
+    """
+
     def __init__(self, state: ServerState, host: str, socket: socket.socket, user_message: List[str], nick: str):
         self.state = state
         self.send_que: queue.Queue[Tuple[str, str] | Tuple[None, None]] = queue.Queue()
         self.socket = socket
         self.host = host
-        self.nick = nick  # Nick is shown in user lists etc, user_name is not
-        self.user_message = " ".join(user_message)  # Ex. AliceUsr 0 * Alice
+        self.nick = nick
+        self.user_message = " ".join(user_message)
         self.user_name = user_message[0]
         self.user_mask = f"{self.nick}!{self.user_name}@{self.host}"
         self.que_thread = threading.Thread(target=self.send_queue_thread)
@@ -151,6 +200,14 @@ class UserConnection:
         self.pong_received = False
 
     def send_queue_thread(self) -> None:
+        """
+        Queue on which the client receives messages from server.
+
+        All messages are a Tuple formatted as (message, prefix).
+        Prefixes are either ":mantatail" or "sender.user_mask"
+
+        A Tuple containing (None, None) indicates a QUIT and closes the connection to the client.
+        """
         while True:
             (message, prefix) = self.send_que.get()
 
@@ -163,8 +220,7 @@ class UserConnection:
                 try:
                     reason = "(Remote host closed the connection)"
                     quit_message = f"QUIT :Quit: {reason}"
-                    # Can be slow, if user has bad internet. Don't do this while holding the lock.
-                    self.send_string_to_client(quit_message, self.user_mask)
+                    self.send_string_to_client(quit_message, self.user_mask)  # Never to be run within the thread Lock
                 except OSError:
                     pass
 
@@ -178,6 +234,7 @@ class UserConnection:
                     self.send_que.put((None, None))
 
     def queue_quit_message_for_other_users(self) -> None:
+        """Alerts all other users that the User has QUIT and closed the connection to the server."""
         # TODO: Implement logic for different reasons & disconnects.
         reason = "(Remote host closed the connection)"
         message = f"QUIT :Quit: {reason}"
@@ -196,6 +253,10 @@ class UserConnection:
             receiver.send_que.put((message, self.user_mask))
 
     def send_string_to_client(self, message: str, prefix: str) -> None:
+        """
+        Takes the message and prefix sent from the server through self.send_que as strings,
+        converts the formatted message to bytes and sends it to the client.
+        """
         try:
             message_as_bytes = bytes(f":{prefix} {message}\r\n", encoding="utf-8")
 
@@ -204,14 +265,32 @@ class UserConnection:
             return
 
     def start_ping_timer(self) -> None:
+        """
+        Starts a timer on a separate thread that when finished sends a PING message to the client
+        to establish that the client still has an open connection to the server.
+        """
         self.ping_timer = threading.Timer(TIMER_SECONDS, self.queue_ping_message)
         self.ping_timer.start()
 
     def queue_ping_message(self) -> None:
+        """
+        Puts PING message in the client's Queue, and starts a new timer waiting for the
+        expected PONG response from the client.
+
+        Ex:
+        Sends ":mantatail PING :mantatail"
+        Expected response: ":Alice!AliceUsr@127.0.0.1 PONG :mantatail"
+        """
         self.send_que.put(("PING :mantatail", "mantatail"))
         threading.Timer(5, self.assert_pong_received).start()
 
     def assert_pong_received(self) -> None:
+        """
+        Uses self.pong_received to assert if the client has sent an appropriate
+        PONG response to the server's PING message.
+
+        If no PONG response has been received, the server closes the connection to the client.
+        """
         if not self.pong_received:
             self.send_que.put((None, None))
         else:
@@ -219,6 +298,10 @@ class UserConnection:
 
 
 class Channel:
+    """
+    Represents an existing channel on the server.
+    """
+
     def __init__(self, channel_name: str, user: UserConnection) -> None:
         self.name = channel_name
         self.founder = user.user_name
@@ -230,15 +313,35 @@ class Channel:
         self.set_operator(user)
 
     def set_operator(self, user: UserConnection) -> None:
+        """
+        Takes a UserConnection object as an argument and adds the user's
+        Nick to the channel's operators.
+        """
         self.operators.add(user.nick.lower())
 
     def remove_operator(self, user: UserConnection) -> None:
+        """
+        Takes a UserConnection object as an argument and removes the user's
+        Nick from the channel's operators.
+        """
         self.operators.discard(user.nick.lower())
 
     def is_operator(self, user: UserConnection) -> bool:
+        """
+        Takes a UserConnection object as an argument and checks if the user's
+        Nick is a channel operator.
+
+        Returns a Boolean.
+        """
         return user.nick.lower() in self.operators
 
     def kick_user(self, kicker: UserConnection, user_to_kick: UserConnection, message: str) -> None:
+        """
+        Puts KICK message in the channel users' Queues notifying them that
+        an operator has kicked a user from the channel.
+
+        Thereafter removes the kicked user from the channel's user Set.
+        """
         for usr in self.users:
             usr.send_que.put((message, kicker.user_mask))
 
@@ -246,6 +349,11 @@ class Channel:
 
 
 def split_on_new_line(string: str) -> List[str]:
+    """
+    Takes string as an argument and splits it on "\r\n" or "\n".
+
+    Returns a List of strings.
+    """
     if string.endswith("\r\n"):
         return string.split("\r\n")
     else:
@@ -253,6 +361,11 @@ def split_on_new_line(string: str) -> List[str]:
 
 
 def get_motd_content_from_json() -> Optional[Dict[str, List[str]]]:
+    """
+    Fetches the server's Message of the Day from 'motd.json'.
+
+    Returns Dict ("String": List)
+    """
     try:
         with open("./resources/motd.json", "r") as file:
             motd_content: Dict[str, List[str]] = json.load(file)
