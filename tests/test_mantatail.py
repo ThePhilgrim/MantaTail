f0cb44d245c43a2c3def79f49e99668009e3465a
Akuli <akuviljanen17@gmail.com>
2022-01-14 17:56:12 +0200

    random.randbytes doesn't exist before python 3.9, use os.urandom instead

--- tests/test_mantatail.py
+++ tests/test_mantatail.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import random
 import socket
@@ -540,6 +541,6 @@ def test_invalid_utf8(user_alice, user_bob):
     while receive_line(user_bob) != b":mantatail 366 Bob #foo :End of /NAMES list.\r\n":
         pass
 
-    random_message = random.randbytes(100).replace(b"\n", b"")
+    random_message = os.urandom(100).replace(b"\n", b"")
     user_alice.sendall(b"PRIVMSG #foo :" + random_message + b"\r\n")
     assert receive_line(user_bob) == b":Alice!AliceUsr@127.0.0.1 PRIVMSG #foo :" + random_message + b"\r\n"
