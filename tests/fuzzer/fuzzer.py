9da8d4a03eae9474ed1aa86e1d5864b9d222959f
Akuli <akuviljanen17@gmail.com>
2022-01-23 23:07:20 +0200

    Add fuzzer script (#95)

---
+++ tests/fuzzer/fuzzer.py
@@ -0,0 +1,46 @@
+"""Simple fuzzer for MantaTail. See https://en.wikipedia.org/wiki/Fuzzing
+
+Usage:
+- Start MantaTail: python3 -m mantatail
+- Run fuzzer in another terminal: python3 fuzzer.py
+- Go back to MantaTail terminal, and see if you get errors.
+"""
+
+import socket
+import random
+
+
+words = [
+    "#bar",
+    "#foo",
+    "+g",
+    "+o",
+    "+xyz",
+    "-g",
+    "-o",
+    "-xyz",
+    "JOIN",
+    "KICK",
+    "MODE",
+    "NICK",
+    "PART",
+    "PING",
+    "PONG",
+    "PRIVMSG",
+    "QUIT",
+    "USER",
+    "",
+]
+
+while True:
+    command = ""
+    for line_number in range(1000):
+        words_per_line = random.randint(1, 5)
+        chosen_words = [random.choice(words) for word_number in range(words_per_line)]
+        command += " ".join(chosen_words) + "\n"
+
+    sock = socket.socket()
+    sock.connect(("localhost", 6667))  # If this fails you forgot to start mantatail
+    sock.sendall(command.encode())
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
