113779084a14822762128af3f83792e3089f55cb
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2022-01-23 22:45:34 +0100

    Run sock.connect in try/except

--- tests/fuzzer/fuzzer.py
+++ tests/fuzzer/fuzzer.py
@@ -8,6 +8,7 @@ Usage:
 
 import socket
 import random
+import sys
 
 
 words = [
@@ -40,7 +41,10 @@ while True:
         command += " ".join(chosen_words) + "\n"
 
     sock = socket.socket()
-    sock.connect(("localhost", 6667))  # If this fails you forgot to start mantatail
-    sock.sendall(command.encode())
-    sock.shutdown(socket.SHUT_RDWR)
-    sock.close()
+    try:
+        sock.connect(("localhost", 6667))
+        sock.sendall(command.encode())
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+    except:
+        sys.exit("Connection Refused: Start Mantatail in a separate terminal before running fuzzer.py")
